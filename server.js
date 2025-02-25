7907f512dffd36fbb4d89edb3f7d7ef32ba4ccfb
Jose David Giron Martinez <jose.giron@bancounion.com>
2025-02-25 09:04:33 -0500

    Repair QR generation

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode');
 const express = require('express');
 const cors = require('cors');
 
